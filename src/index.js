8abf383583c0463288196a1332fe71b093d63173
Travis Vela <jtravisvela@gmail.com>
2019-03-19 13:44:09 -0500

    add npm, basic structure

--- src/index.js
+++ src/index.js
@@ -1,20 +1,61 @@
 /**
  * es6 modules and imports
  */
-import sayHello from './hello';
-sayHello('World');
+// import sayHello from './hello';
+// sayHello('World');
+
 
 /**
  * require style imports
  */
+
+'use strict';
+
 const {getMovies} = require('./api.js');
 
-getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
-  });
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-  console.log(error);
-});
+const movieFetch = () => getMovies().then((movies) => {
+    console.log('Here are all the movies:');
+    movies.forEach(({title, rating, id}) => {
+        $('#movies').append(movieDiv(title, rating, id));
+
+        console.log(`id#${id} - ${title} - rating: ${rating}`);
+
+    });
+    }).catch((error) => {
+        alert('Oh no! Something went wrong.\nCheck the console for details.')
+        console.log(error);
+    });
+
+
+movieFetch();
+
+const starRating = (rating) => {
+    let star = '';
+    for (let i = 0; i < rating; i++) {
+        star += '<i class="fas fa-star"></i>'
+    }
+    return star;
+};
+
+const movieDiv = (title, rating, id) => {
+    let star = starRating(rating);
+    let html = `<div class="col-12 col-md-6">
+        <h3>${title}</h3>
+        <p>${star}</p>
+        </div>`;
+
+    return html;
+};
+
+const addMovie = () => {
+    $('#submit').on('click', function() {
+        console.log('you clicked the submit button');
+        const newMovie = {
+            title: $('#title').val(),
+            rating: $('input[name = rating]:checked').val()
+        }
+    })
+};
+
+
+
